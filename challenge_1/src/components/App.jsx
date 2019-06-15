3258bbe93d95d36903d3f0dad535b1e20d850f81
Chad Cramer <chcr6493@colorado.edu>
2019-06-14 19:44:54 -0500

    asdf

--- challenge_1/src/components/App.jsx
+++ challenge_1/src/components/App.jsx
@@ -2,10 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { username: undefined }
+  }
+
+    handleText (e) {
+      if(e.key === 'Enter'){
+        console.log(e.target.value);
+        e.target.value = '';
+      }
+    }
+
     render () {
       return (
       <div>
-        Hello world
+        Search for History
+        <div>
+          <input type="text" placeholder="Search.." onKeyDown={this.handleText} />
+        </div>
       </div>
       )
     }
